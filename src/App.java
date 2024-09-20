fdc2134d4fcf4af91208fdeb78c8d7b95a196ee4
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-09-20 08:00:57 +0000

    List of Student

--- src/App.java
+++ src/App.java
@@ -7,10 +7,15 @@ public class App {
         System.out.println("Hello, World!");
 
         ArrayList<Student> sl = new ArrayList<Student>();
-        Student s = new Student("test", "abc");
-        sl.add(s);
+        Student s1 = new Student("Nguyen Thi Lan", "msv0123");
+        Student s2 = new Student("Tran Van Minh", "msv12345");
+
+        sl.add(s1);
+        sl.add(s2);
 
         System.out.println(sl.get(0).fullname);
-
+        System.out.println(sl.get(0).studentId);
+        System.out.println(sl.get(1).fullname);
+        System.out.println(sl.get(1).studentId);
     }
 }

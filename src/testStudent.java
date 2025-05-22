02665565b49ad9183c31e649357abe4c5fbf994f
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2025-05-22 11:48:26 +0000

    test

--- src/testStudent.java
+++ src/testStudent.java
@@ -1,54 +1,39 @@
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class testStudent {
 
-    public void test(){
+    public void test() {
 
-         ArrayList<Student> sl = new ArrayList<Student>();
+        ArrayList<Student> sl = new ArrayList<Student>();
         Student s1 = new Student("Nguyen Thi Lan Anh", 12345);
         Student s2 = new Student("Tran Van Minh", 1);
 
         sl.add(s1);
         sl.add(s2);
-        /* 3
-        System.out.println(sl.get(0).fullname);
-        System.out.println(sl.get(0).studentId);
-        System.out.println(sl.get(1).fullname);
-        System.out.println(sl.get(1).studentId); 
-        
-        */
 
         StudentList stuList = new StudentList();
         stuList.addStudent(s1);
         stuList.addStudent(s2);
 
-        //cap nhat thong tin
-        
-        System.out.println("Enter student ID");
-        Scanner studentID = new Scanner(System.in) ;
+        // cap nhat thong tin
 
-int s = studentID.nextInt();
-       
-    
+        System.out.println("Enter student ID");
+        Scanner studentID = new Scanner(System.in);
+
+        int s = studentID.nextInt();
 
         System.out.println("Enter student fullname");
 
-        Scanner fullname = new Scanner(System.in);  // Create a Scanner object
+        Scanner fullname = new Scanner(System.in); // Create a Scanner object
 
         String newName = fullname.nextLine();
-      
-      
 
         stuList.getEditStudent(newName, s);
-       
-        stuList.printStudentList();
-        
 
+        stuList.printStudentList();
 
     }
-    
+
 }

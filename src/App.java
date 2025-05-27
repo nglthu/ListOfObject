54116d7869ce6286b910162d32b3512a517add2e
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2025-05-27 10:03:52 +0000

    minimise code with Reference

--- src/App.java
+++ src/App.java
@@ -9,6 +9,7 @@ public class App {
 
 
         Student st = new Student("Le Thu Nguyen", 10);
+        Student s2 = new Student("Le Van Hung", 99);
 
         Book b = new Book("dkdkdk",100);
 
@@ -17,6 +18,7 @@ public class App {
 
         ol.addObject(b);
         ol.addObject(st);
+        ol.addObject(s2);
        
 
         ol.printObject();

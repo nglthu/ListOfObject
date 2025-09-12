1e2401800282469f5bea9ae1fc7a532479624fc5
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2025-09-12 08:00:24 +0000

    defactor

--- src/App.java
+++ src/App.java
@@ -1,18 +1,12 @@
 
-
 public class App {
     public static void main(String[] args) throws Exception {
 
-      // testStudentSingle.test();
-       //   testGeneric.test();
-      // testCallback.test();
+        // testStudentSingle.test();
+        // testGeneric.test();
+        // testCallback.test();
 
-      testLibrary.test();
+        testLibrary.test();
 
-      
-
-
-     
-
-}
+    }
 }
